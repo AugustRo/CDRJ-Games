ca403cbfe3d9066cf6485f6d88b46b451970a448
AugustRo <64295693+AugustRo@users.noreply.github.com>
2021-04-21 19:54:52 -0500

    Mejoras diseño

--- src/routes/juegos.js
+++ src/routes/juegos.js
@@ -3,12 +3,15 @@ const router = express.Router();
 //Conexion a la BD
 const pool = require('../database');
 
-router.get('/add',(req, res) => {
-    res.render('juegos/add');
-});
+//router.get('/add',(req, res) => {
+    //res.render('juegos/add');
+//});
 
-router.post('/add',(req, res) => {
-    res.send('recibido');
+//router.post('/add',(req, res) => {
+  //  res.send('recibido');
+//});
+router.get('/', (req, res) => {
+    res.render('juegos/list');
 });
 
 module.exports = router;
