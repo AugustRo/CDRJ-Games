63a92b7ff06c698750a1a4672567f2d4b4bab885
AugustRo <64295693+AugustRo@users.noreply.github.com>
2021-04-28 22:03:59 -0500

    modificacion de keys

--- src/keys.js
+++ src/keys.js
@@ -1,14 +1,8 @@
 module.exports = {
     host:'localhost', 
-<<<<<<< HEAD
     //usuario
     user: 'cdjrgames',
     //contraseña
     password: 'games123',
     database: 'database_cdjr_games'
-=======
-    user: 'root',
-    password: '',
-    database: 'database_links'
->>>>>>> 737d1b053c68d9ed7027e0ca56e48f61f0f89278
 }
