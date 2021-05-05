4dd70224436a0435fa502cb650be3772a24ec3e2
Etzary <jvillegaspan@gmail.com>
2021-05-04 17:00:56 -0700

    Arreglo archivos x2 jajas

--- src/routes/juegos.js
+++ src/routes/juegos.js
@@ -1,41 +1,21 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const router = express.Router();
-const fs = require('fs');
-const busboy = require('connect-busboy');
+const util = require('util');
+const path = require('path');
+
+
+const app = express();
+
+
+app.use(fileUpload())
 //Conexion a la BD
 const pool = require('../database');
-router.use(busboy());
-router.use(fileUpload());
-
-/*router.post('/add', function(req, res) {
-  var fstream;
-  console.log(req.body);
-  req.pipe(req.busboy);
-  req.busboy.on('file', function (fieldname, file, filename) {
-      console.log("Uploading: " + filename); 
-      fstream = fs.createWriteStream(__dirname + '/../catalogo/' + filename);
-      file.pipe(fstream);
-      fstream.on('close', function () {
-          res.redirect('back');
-      });
-  });
-  console.log(req.busboy.filename);
-});
-*/
-router.post('/add', (req, res) => {
-  if(req.files){
-    console.log(req.files)
-    var file = req.files.img
-    var filename = img.name
-    console.log(filename)
-  }
-});
-
 
 //Ruta para entrar en Add juegos
 router.get('/add', (req, res) => {
   res.render('juegos/add');
+    res.render('juegos/add');
 });
 
 //Ruta que lee los juegos
