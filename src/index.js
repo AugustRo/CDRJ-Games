7c3bbab2e8c42aa3d8fa8fc948d3e3f0fe7196b9
R_Giron_C <ricardo.giron.colorado@gmail.com>
2021-05-12 13:47:05 -0500

    Sign Up y Sign In

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ const { end } = require('./database');
 const fs = require('fs');
 const busboy = require('connect-busboy');
 
+const { database } = require('./keys');
+
+const session = require('express-session');
+const validator = require('express-validator');
+const passport = require('passport');
+const flash = require('connect-flash');
+const MySQLStore = require('express-mysql-session')(session);
+const bodyParser = require('body-parser');
+
 //Inicializar
 const app = express();
 app.use(fileUpload({
@@ -30,6 +39,19 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(session({
+    secret: 'CDRJ_GAMES',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore(database)
+  }));
+
+
+
 //Variables globales
 app.use((req,res, next) => {
     next();
@@ -40,12 +62,20 @@ app.use(require('./routes'));
 app.use(require('./routes/authentication'));
 app.use('/juegos',require('./routes/juegos'));
 app.use('/login',require('./routes/login'));
-app.use('/signup',require('./routes/signup'));
+app.use('/signup', require('./routes/signup'));
 app.use('/mi_lista', require('./routes/mi_lista'));
+app.use('/profile', require('./routes/profile'));
 //Archivos publicos
 app.use(express.static(path.join(__dirname,'public')));
 
 //Comenzar servidor
 app.listen(app.get('port'), ()=>{
     console.log('Servidor en el puerto:',app.get('port'));
+});
+
+//Variables globales
+app.use((req, res, next) => {
+    app.locals.success = req.flash('success');
+    app.locals.success = req.flash('message');
+    next();
 });
--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,29 +1,59 @@
 const express = require('express');
 const router = express.Router();
+
+const passport = require ('passport');
+const LocalStrategy = require ('passport-local').Strategy;
 //Conexion a la BD
 const pool = require('../database');
+const helpers = require('../lib/helpers');
+
+//SIGN UP
+
+passport.use('user.signup', new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'passw',
+  passReqToCallback: true
+}, async(req, username, passw, done) => {
+  const {nombre, apellido, email} = req.body;
+  const  newUser = {
+  nombre,
+  apellido,
+  username,
+  email, 
+  passw  
+  };
+  console.log(newUser);
+  
+  newUser.passw = await helpers.encryptPassword(passw);
+
+  const result = await pool.query('INSERT INTO users set ?', [newUser]);
+
+  newUser.id = result.insertId;
+  return done(null, newUser);
+}));
 
 router.get('/add',(req, res) => {
-    res.render('user/signup');
-});
-
-router.post('/add', async (req, res) => {
-    const {nombre, apellido, username, email, passw} = req.body;
-    const  newUser = {
-    nombre,
-    apellido,
-    username,
-    email, 
-    passw  
-    };
-    console.log(newUser);
-    
-    await pool.query('INSERT INTO users set ?', [newUser]);
-    res.render('index');
+  res.render('user/signup');
 });
 
 router.get('/', (req, res) => {
   res.render('user/signup.hbs');
+});
+
+
+router.post('/add', passport.authenticate('user.signup', {
+  successRedirect: '/profile',
+  failureRedirect: '/signup',
+  failureFlash: true
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const rows = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
+  done(null, rows[0]);
 });
 
 module.exports = router;
